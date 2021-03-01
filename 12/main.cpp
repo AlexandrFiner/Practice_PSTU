2ba182f3beb2d8081d8eb168a0479077d535ec49
AlexandrFiner-macbook <finersanya@gmail.com>
2021-03-01 14:20:36 +0500

    Обновленное меню, поиск

--- 12/main.cpp
+++ 12/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <ctime>
 
 using namespace std;
 
@@ -59,46 +60,52 @@ string pasport_fake[FAKE_DATA] = {
 };
 
 struct State {
-    string name;        // Фио
-    string birthday;    // Дата рождения
-    string pasport;     // Паспорт
+    string name;            // Фио
+    string birthday;        // Дата рождения
+    int birthday_weight;    // 'вес' даты
+    string pasport;         // Паспорт
 };
 
 struct Node {
-    State data;  // Элемент списка
-    Node* next; // Указатель на следующий элемент
+    State data;     // Элемент списка
+    Node* next;     // Указатель на следующий элемент
+    Node* prev;     // Указатель на прошлый элемент
 };
 
 Node* make();
 void print(Node* first);
+void searchLinear(Node* first);
+int parseDate(string &input);
+void sort(Node* first);
 
 int main() {
     srand(time(NULL));
     setlocale(LC_ALL, "Rus");
 
     Node* list = make();
-    print(list);
 
     int choice;
     do {
         cout << "Main Menu" << endl;
-        cout << "1 - Генерация списка" << endl;
+        cout << "1 - Генерация списка (not yet)" << endl;
         cout << "2 - Вывести текущий список" << endl << endl;
-        cout << "3 - Поиск" << endl;
+        cout << "3 - Линейный поиск" << endl;
+        cout << "4 - Интерполяционный поиск" << endl;
+        cout << "5 - Прямой поиск подстроки в строке" << endl;
         cout << endl << endl << "0 - ВЫХОД" << endl;
         cin >> choice;
+        system("clear");
         switch (choice) {
+            case 0:
+                break;
             case 1:
                 break;
             case 2:
+                print(list);
                 break;
-            default:
-                cout << "Main Menu" << endl;
-                cout << "1 - Генерация списка" << endl;
-                cout << "2 - Вывести текущий список" << endl << endl;
-                cout << "3 - Поиск" << endl;
-                cout << endl << endl << "0 - ВЫХОД" << endl;
-                cin >> choice;
+            case 3:
+                searchLinear(list);
+                break;
         }
     } while (choice != 0);
 }
@@ -110,19 +117,24 @@ Node* make() {
     p = new Node;
     p->data.name = name_fake[rand() % FAKE_DATA];
     p->data.birthday = birthday_fake[rand() % FAKE_DATA];
+    p->data.birthday_weight = parseDate(p->data.birthday);
     p->data.pasport = pasport_fake[rand() % FAKE_DATA];
     p->next = NULL;
+    p->prev = NULL;
 
     first = p;
 
     for(int i = 2; i <= RND_CNT; i++) {
         Node* h = new Node; // Память под новый элемент
+        Node* q = p;
         p->next = h;
-        p = p->next;        // Перешли к управлению над след элементом, так сказать :)
+        p = p->next;  // Перешли к управлению над след элементом, так сказать :)
         p->data.name = name_fake[rand() % FAKE_DATA];
         p->data.birthday = birthday_fake[rand() % FAKE_DATA];
+        p->data.birthday_weight = parseDate(p->data.birthday);
         p->data.pasport = pasport_fake[rand() % FAKE_DATA];
         p->next = NULL; // Это последний элемент
+        p->prev = q;
     }
 
     return first;
@@ -139,5 +151,87 @@ void print(Node* first) {
             cout << "Паспорт: " << p->data.pasport << endl;
             p = p->next;
         }
+        int out = 1;
+        while(out != 0) {
+            cout << endl << "0 - назад" << endl;
+            cin >> out;
+        }
+        system("clear");
     }
+}
+
+void searchLinear(Node* first) {
+    if(first == NULL) {
+        cout << "Список пуст" << endl;
+    } else {
+        system("clear");
+        string search;
+        cout << "Введите дату рождения для поиска" << endl;
+        cin >> search;
+
+
+        unsigned int start_time =  clock(); // начальное время
+        int search_count = 0;
+        Node* p = first;
+        while (p != NULL) {
+            search_count += 1;
+            if(p->data.birthday == search) {
+                cout << "ФИО: " << p->data.name << " | ";
+                cout << "Дата рождения: " << p->data.birthday << " | ";
+                cout << "Паспорт: " << p->data.pasport << endl;
+            }
+            p = p->next;
+        }
+        unsigned int end_time = clock(); // конечное время
+        unsigned int search_time = end_time - start_time; // искомое время
+
+        cout << endl << "Debug:";
+        cout << endl << "Пройдено сравнений: " << search_count;
+        cout << endl << "Время выполнения функции: " << search_time/1000.0 << " мс." << endl;
+
+        int out = 1;
+        while(out != 0) {
+            cout << endl << "0 - назад" << endl;
+            cin >> out;
+        }
+        system("clear");
+    }
+}
+
+void search(Node* first) {
+    if(first == NULL) {
+        cout << "Список пуст" << endl;
+    } else {
+
+    }
+}
+
+void sort(Node* first) {
+    if(first == NULL) {
+        cout << "Список пуст" << endl;
+    } else {
+        Node* p = first;
+
+        while(p != NULL) {
+            p = p->next;
+        }
+    }
+}
+
+void sort(list **first)
+{
+
+}
+
+int parseDate(string &input) {
+    int day;
+    int month;
+    int year;
+    if (sscanf(input.c_str(), "%d.%d.%d", &day, &month, &year) != 3) {
+        // обработка ошибок
+        // но пока тут ничего нет :)
+    } else {
+        return 100 * year + 10 * month + day;
+    }
+    return 0;
 }
